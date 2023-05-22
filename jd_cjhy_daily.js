4e1ef29f9d6389954bae5493aaa56dc7cadb257d
chlchl1 <106399756+chlchl1@users.noreply.github.com>
2023-05-23 01:12:44 +0800

    Update jd_cjhy_daily.js

--- jd_cjhy_daily.js
+++ jd_cjhy_daily.js
@@ -8,8 +8,6 @@ export jd_cjhy_daily_openCard="1"            设置为1则自动入会 不设置
 
 cron "2 2 29 2 *" jd_cjhy_daily.js
 */
-require("global-agent/bootstrap");
-global.GLOBAL_AGENT.HTTP_PROXY="http://172.17.0.1:8899";
 const $ = new Env('cjhy每日抢')
 
 
